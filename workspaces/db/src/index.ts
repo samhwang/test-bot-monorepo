c11be30c7b78bab1fc7ff800595c165804c321cd
Sam Huynh <samhwang2112.dev@gmail.com>
2023-08-06 01:34:51 +1000

    db: export prisma object

--- workspaces/db/src/index.ts
+++ workspaces/db/src/index.ts
@@ -1,3 +1,10 @@
-export function sayHello() {
-  console.log('Hello World!');
+import { PrismaClient } from "@prisma/client";
+
+export * from "@prisma/client";
+
+const globalForPrisma = globalThis as { prisma?: PrismaClient };
+export const prisma = globalForPrisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
 }
